89b4b175a8400db190c558ed96392b2d7a064621
maximkondrashuk <mkondras@ucsd.edu>
2023-12-30 16:53:26 -0800

    home.js padding and margins

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,8 @@ const Home = () => {
     const [gradientStart, setGradientStart] = useState('100%');
     const imageRef = useRef(null);
 
-    {/* TODO: FIX HEIGHT VARIABLE */}
-
     useEffect(() => {
         if (imageRef.current) {
-            const height = imageRef.current.clientHeight;
             const aspectRatio = 2.35 / 1;
             const imageHeight = imageRef.current.offsetWidth / aspectRatio;
             setGradientStart(imageHeight);
@@ -26,12 +23,13 @@ const Home = () => {
                     backgroundPosition: 'top right, top',
                     minHeight: '100vh' }}>
 
-                    <div className="bg-white float-right" style={{ width: '80%', height: `${gradientStart+50}px`, shapeOutside: 'circle(35% at 60% 30%)', clipPath: 'circle(35% at 60% 30%)' }}></div>
+                    <div className="float-right" style={{ width: '80%', height: `${gradientStart+50}px`, shapeOutside: 'circle(40% at 60% 30%)' }}></div>
+                    {/* , clipPath: 'circle(40% at 60% 30%)' */}
 
                     {/* TODO: FIX PADDING */}
 
-                    <div className="pt-48 pl-16">
-                        <p className="">My name is Maxim. I graduated from UC San Diego in March 2023 with a Bachelor of 
+                    <div className="text-justify pt-1-12 mx-1/7 space-y-16">
+                        <p>My name is Maxim. I graduated from UC San Diego in March 2023 with a Bachelor of 
                             Science in Computer Science. With a strong academic record, I blend a rigorous 
                             educational background with hands-on experience in software engineering. Embarking 
                             on a comprehensive exploration of computer science, my journey has equipped me with 
@@ -39,23 +37,24 @@ const Home = () => {
                             assurance of technological robustness. This experience has granted me a profound 
                             insight into the interplay between theoretical foundations and their practical 
                             applications in the tech landscape.</p>
-                        <p className="pt-8">During my internship at General Atomics, I engaged deeply with flight-critical software, 
+                        <p>During my internship at General Atomics, I engaged deeply with flight-critical software, 
                             sharpening my analytical skills and playing a pivotal role in enhancing system safety 
                             and performance. My proactive approach to problem-solving and meticulous attention to 
                             code quality were instrumental in accelerating software delivery and strengthening system 
                             robustness.</p>
-                        <p className="pt-8">Since graduating from UC San Diego, I have immersed myself in a variety of projects that 
+                        <p>Since graduating from UC San Diego, I have immersed myself in a variety of projects that 
                             have broadened my technical expertise and provided extensive hands-on experience. This 
                             continuous journey of learning and application has not only solidified my proficiency in 
                             a range of technologies but also honed my ability to craft dynamic solutions that meet 
                             complex challenges. My commitment to technology extends beyond academic knowledge, embracing 
                             the practical intricacies of software development and the innovative spirit of the industry.</p>
-                        <p className="pt-8">As a technology enthusiast and proactive problem solver, I am proficient in a variety of 
+                        <p>As a technology enthusiast and proactive problem solver, I am proficient in a variety of 
                             programming languages and tools. With a readiness to embrace challenges and a drive to 
                             extend the limits of what's possible in software development, I am eager to collaborate 
                             and innovate.</p>
                     </div>
-                    <div className="pt-24 pl-16">
+
+                    <div className="text-center pt-1-12 mx-1/7 space-y-16">
                         <h2>Skills</h2>
                         <p>Java, C, C++, JavaScript, Python, Scikit-learn, PyTorch, TensorFlow, HTML/CSS, React, Node.js, PostgreSQL</p>
                     </div>
